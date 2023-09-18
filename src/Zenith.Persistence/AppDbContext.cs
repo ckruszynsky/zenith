01bcc848cd03d7361c10723807cd64e17218be43
Chad Kruszynsky <e51779@lahsic.com>
2023-09-18 15:35:10 -0500

    added persistence project and entity configuration

---
+++ src/Zenith.Persistence/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Zenith.Domain.Entities;
+using Zenith.SharedKernel;
+
+namespace Zenith.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<ZenithUser>
+    {
+        private readonly IDomainEventDispatcher? _dispatcher;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options, IDomainEventDispatcher? dispatcher = null) : base(options)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ArticleTag> ArticleTags { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<UserFollow> UserFollows { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            base.OnModelCreating(builder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            if (_dispatcher == null)
+            {
+                return result;
+            }
+
+            var entitiesWithEvents = ChangeTracker.Entries<EntityBase>()
+                .Select(e => e.Entity)
+                .Where(e => e.DomainEvents.Any())
+                .ToArray();
+
+            await _dispatcher.DispatchAndClearEvents(entitiesWithEvents);
+
+            return result;
+        }
+
+        public override int SaveChanges()
+        {
+            return SaveChangesAsync().GetAwaiter().GetResult();
+        }
+
+    }
+}
---
+++ src/Zenith.Persistence/Configurations/ActivityLogConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Zenith.Domain.Entities;
+
+namespace Zenith.Persistence.Configurations
+{
+    public class ActivityLogConfiguration : IEntityTypeConfiguration<ActivityLog>
+    {
+        public void Configure(EntityTypeBuilder<ActivityLog> builder)
+        {
+            builder.Property(a => a.TransactionType)
+                .HasConversion(new EnumToStringConverter<TransactionType>());
+        }
+    }
+}
