51452da389e0493347146c39b780ea0e64665f5e
Chad Kruszynsky <e51779@lahsic.com>
2023-09-19 11:19:48 -0500

    add initializer and refactored

--- src/Zenith.Persistence/AppDbContext.cs
+++ src/Zenith.Persistence/AppDbContext.cs
@@ -25,16 +25,5 @@ namespace Zenith.Persistence
             base.OnModelCreating(builder);
         }
 
-        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-        {
-            int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-            return result;
-        }
-
-        public override int SaveChanges()
-        {
-            return SaveChangesAsync().GetAwaiter().GetResult();
-        }
-
     }
 }
