a5c3ca916a42ba181dd1e8892d95c7d4373c15b3
Chad Kruszynsky <ckruszynsky@gmail.com>
2023-09-25 10:36:21 -0500

    Feat/user commands (#4)
    * merge conflict
    
    * added update user command handler
    
    ---------
    
    Co-authored-by: Chad Kruszynsky <e51779@lahsic.com>

--- tests/Zenith.Core.Tests/Users/CreateUserCommandHandlerTest.cs
+++ tests/Zenith.Core.Tests/Users/CreateUserCommandHandlerTest.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging.Abstractions;
 using Shouldly;
+using Zenith.Core.Domain.Entities;
+
 using Zenith.Core.Features.Users;
 using Zenith.Core.Tests.Infrastructure;
 
@@ -34,6 +37,73 @@ namespace Zenith.Core.Tests.Users
             result.Value.UserName.ShouldBe(createUserCommand.Username);
             result.Value.Email.ShouldBe(createUserCommand.Email);
             Context.Users.Single(u => u.UserName == createUserCommand.Username).ShouldNotBeNull();
+        }
+
+
+        [Fact]
+        public async Task GivenRequestContainsExistingEmail_WhenUserAlreadyExists_ReturnsError()
+        {
+            var createUserCommand = new CreateUser.Command
+            (
+                "testEmail@gmail.com",
+                "testUsername1",
+                "#testPassword1!"
+            );
+
+            var existingUserWithSameUsername = new ZenithUser
+            {
+                Email = createUserCommand.Email,
+                NormalizedEmail = createUserCommand.Email.ToUpperInvariant(),
+                UserName = "testUserName12",
+                NormalizedUserName = "testUserName12".ToLowerInvariant(),
+                SecurityStamp = "someRandomSecurityStamp"
+            };
+
+            existingUserWithSameUsername.PasswordHash = new PasswordHasher<ZenithUser>()
+            .HashPassword(existingUserWithSameUsername, "password");
+
+            await UserManager.CreateAsync(existingUserWithSameUsername);
+
+            var command = new CreateUser.Handler(_logger, UserManager, Context, Mapper, TokenService);
+
+            var result = command.Handle(createUserCommand, CancellationToken.None);
+
+            result.Result.IsSuccess.ShouldBeFalse();
+            result.Result.Errors.ShouldNotBeEmpty();
+            result.Result.Errors.ShouldContain($"Email {createUserCommand.Email} is already in use");
+        }
+
+        [Fact]
+        public async Task GivenRequestContainsExistingUsername_WhenUserAlreadyExists_ReturnsError()
+        {
+            var createUserCommand = new CreateUser.Command
+            (
+                "testEmail@gmail.com",
+                "testUsername1",
+                "#testPassword1!"
+            );
+
+            var existingUserWithSameUsername = new ZenithUser
+            {
+                Email = "testEmail1@gmail.com",
+                NormalizedEmail = "testEmail1@gmail.com".ToUpperInvariant(),
+                UserName = createUserCommand.Username,
+                NormalizedUserName = createUserCommand.Username.ToLowerInvariant(),
+                SecurityStamp = "someRandomSecurityStamp"
+            };
+
+            existingUserWithSameUsername.PasswordHash = new PasswordHasher<ZenithUser>()
+            .HashPassword(existingUserWithSameUsername, "password");
+
+            await UserManager.CreateAsync(existingUserWithSameUsername);
+
+            var command = new CreateUser.Handler(_logger, UserManager, Context, Mapper, TokenService);
+
+            var result = command.Handle(createUserCommand, CancellationToken.None);
+
+            result.Result.IsSuccess.ShouldBeFalse();
+            result.Result.Errors.ShouldNotBeEmpty();
+            result.Result.Errors.ShouldContain($"Username {createUserCommand.Username} is already in use");
         }
     }
 }
---
+++ tests/Zenith.Core.Tests/Users/UpdateUserCommandHandlerTest.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Shouldly;
+using Zenith.Core.Domain.Entities;
+using Zenith.Core.Features.Users;
+using Zenith.Core.Tests.Infrastructure;
+
+namespace Zenith.Core.Tests.Users
+{
+    [Collection("ZenithCollectionFixture")]
+    public class UpdateUserCommandHandlerTest : TestFixture
+    {
+        private ILogger<UpdateUser.Handler> _logger;
+
+        public UpdateUserCommandHandlerTest()
+        {
+            _logger = NullLogger<UpdateUser.Handler>.Instance;
+        }
+
+        [Fact]
+        public async void GivenValidUserRequest_WhenTheUserExistsAndUpdatesEmail_ReturnsUpdateUserViewModelResponseAndIssueNewToken()
+        {
+            var updateUserCommand = new UpdateUser.Command
+            (
+                "aUpdatedEmail@gmail.com",
+                TestConstants.TestUserName,
+                "This is my bio",
+                "A new image"
+            );
+
+            var originalUser = await UserManager.FindByEmailAsync(TestConstants.TestUserEmail);
+
+            var command = new UpdateUser.Handler(CurrentUserContext, Mapper, UserManager, Context, TokenService);
+            var result = await command.Handle(updateUserCommand, CancellationToken.None);
+
+            result.ShouldNotBeNull();
+            result.Value.ShouldNotBeNull();
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.Email.ShouldBe(updateUserCommand.Email);
+            result.Value.UserName.ShouldBe(updateUserCommand.Username);
+            result.Value.Bio.ShouldBe(updateUserCommand.Bio);
+            result.Value.Image.ShouldBe(updateUserCommand.Image);
+
+            result.Value.Token.ShouldNotBe(new CurrentUserContextTest(UserManager).GetCurrentUserToken());
+            result.Value.Token.ShouldBe(new TokenServiceTest().CreateToken(originalUser));
+
+
+        }
+
+        [Fact]
+        public async Task GivenValidUserRequest_WhenTheUserExistsAndUpdatesUsername_ReturnsUpdateUserViewModelResponseAndIssuesNewToken()
+        {
+            var updateUserCommand = new UpdateUser.Command
+            (
+               Username: "aupdatedUsername",
+               Bio: "my bio",
+               Image: "a new image"
+            );
+
+            var originalUser = await UserManager.FindByEmailAsync(TestConstants.TestUserEmail);
+
+            var command = new UpdateUser.Handler(
+                CurrentUserContext,
+                Mapper,
+                UserManager,
+                Context,
+                TokenService
+               );
+
+            var response = await command.Handle(updateUserCommand, CancellationToken.None);
+
+            response.Value.ShouldNotBeNull();
+            response.Value.Email.ShouldBe(TestConstants.TestUserEmail);
+            response.Value.UserName.ShouldBe(updateUserCommand.Username);
+            response.Value.Bio.ShouldBe(updateUserCommand.Bio);
+            response.Value.Image.ShouldBe(updateUserCommand.Image);
+
+            response.Value.Token.ShouldNotBe(new CurrentUserContextTest(UserManager).GetCurrentUserToken());
+            response.Value.Token.ShouldBe(new TokenServiceTest().CreateToken(originalUser));
+
+        }
+
+        [Fact]
+        public async Task GivenValidUserRequest_WhenTheUserExistsAndDoesNotUpdateUsernameOrEmail_ReturnsUpdateUserViewModelResponseWithSameToken()
+        {
+            var updateUserCommand = new UpdateUser.Command(Bio: "My Bio", Image: "a new image");
+
+            var originalUser = await UserManager.FindByEmailAsync(TestConstants.TestUserEmail);
+
+            var handler = new UpdateUser.Handler(
+                CurrentUserContext,
+                Mapper,
+                UserManager,
+                Context,
+                TokenService
+                );
+
+            var response = await handler.Handle(updateUserCommand, CancellationToken.None);
+
+            response.Value.ShouldNotBeNull();
+            response.IsSuccess.ShouldBeTrue();
+            response.Value.Email.ShouldBe(TestConstants.TestUserEmail);
+            response.Value.UserName.ShouldBe(TestConstants.TestUserName);
+            response.Value.Bio.ShouldBe(updateUserCommand.Bio);
+            response.Value.Image.ShouldBe(updateUserCommand.Image);
+
+            response.Value.Token.ShouldBe(new CurrentUserContextTest(UserManager).GetCurrentUserToken());
+            response.Value.Token.ShouldNotBe(new TokenServiceTest().CreateToken(originalUser));
+
+        }
+
+        [Fact]
+        public async Task GivenValidUserRequest_WhenTheUpdatedUsernameAlreadyExists_ReturnsErrorResponse()
+        {
+            var updateUserCommand = new UpdateUser.Command(Username: "existing.user");
+
+            var existingUserWithSameUsername = new ZenithUser
+            {
+                Email = "existingUser@gmail.com",
+                NormalizedEmail = "existingUser@gmail.com".ToUpperInvariant(),
+                UserName = updateUserCommand.Username,
+                NormalizedUserName = updateUserCommand.Username.ToUpperInvariant(),
+                SecurityStamp = "someRandomSecurityStamp"
+            };
+
+            existingUserWithSameUsername.PasswordHash = new PasswordHasher<ZenithUser>()
+        .HashPassword(existingUserWithSameUsername, "password");
+
+            await UserManager.CreateAsync(existingUserWithSameUsername);
+
+
+            var command = new UpdateUser.Handler(
+                CurrentUserContext,
+                Mapper,
+                UserManager,
+                Context,
+                TokenService);
+
+            var response = await command.Handle(updateUserCommand, CancellationToken.None);
+
+            response.IsSuccess.ShouldBeFalse();
+            response.Errors.ShouldContain($"Username {updateUserCommand.Username} is already in use");
+        }
+
+        [Fact]
+        public async Task GivenValidUserRequest_WhenTheUpdatedEmailAlreadyExists_ReturnsErrorResponse()
+        {
+            var updateUserCommand = new UpdateUser.Command(Email: "existingUser@gmail.com");
+
+            var existingUserWithSameUsername = new ZenithUser
+            {
+                Email = "existingUser@gmail.com",
+                NormalizedEmail = "existingUser@gmail.com".ToUpperInvariant(),
+                UserName = "existing.user",
+                NormalizedUserName = "existing.user".ToUpperInvariant(),
+                SecurityStamp = "someRandomSecurityStamp"
+            };
+
+            existingUserWithSameUsername.PasswordHash = new PasswordHasher<ZenithUser>()
+        .HashPassword(existingUserWithSameUsername, "password");
+
+            await UserManager.CreateAsync(existingUserWithSameUsername);
+
+
+            var command = new UpdateUser.Handler(
+                CurrentUserContext,
+                Mapper,
+                UserManager,
+                Context,
+                TokenService);
+
+            var response = await command.Handle(updateUserCommand, CancellationToken.None);
+
+            response.IsSuccess.ShouldBeFalse();
+            response.Errors.ShouldContain($"Email {updateUserCommand.Email} is already in use");
+        }
+
+        [Fact]
+        public async Task GivenValidUserRequest_WhenRequestContainsNewPassword_ReturnsSuccessResponse()
+        {
+            var updateUserCommand = new UpdateUser.Command(Password: "aNewPassword!124#");
+
+            var originalUser = await UserManager.FindByEmailAsync(TestConstants.TestUserEmail);
+
+            var handler = new UpdateUser.Handler(
+                CurrentUserContext,
+                Mapper,
+                UserManager,
+                Context,
+                TokenService);
+
+            var response = await handler.Handle(updateUserCommand, CancellationToken.None);
+
+            response.IsSuccess.ShouldBeTrue();
+            response.Errors.ShouldBeEmpty();
+            response.Value.ShouldNotBeNull();
+            response.Value.ShouldBeOfType<UserViewModel>();
+
+            var updatedUser = await UserManager.FindByEmailAsync(TestConstants.TestUserEmail);
+            updatedUser.PasswordHash.ShouldNotBeNullOrWhiteSpace();
+
+            var validatedPassword = await UserManager.CheckPasswordAsync(originalUser, updateUserCommand.Password);
+            var invalidOldPassword = await UserManager.CheckPasswordAsync(originalUser, "#password1!");
+
+            validatedPassword.ShouldBeTrue();
+            invalidOldPassword.ShouldBeFalse();
+        }
+    }
+}
+
